9ce5b2717e9a3050876dfcd96c0cadebd5f0cd93
MatkoTom <tomljanovic.matko@gmail.com>
2019-07-28 12:09:41 +0200

    Initial commit

---
+++ app/src/main/java/com/example/summerschoolapp/view/RegisterScreen.java
@@ -0,0 +1,18 @@
+package com.example.summerschoolapp.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.summerschoolapp.R;
+
+import java.util.ArrayList;
+
+public class RegisterScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_screen);
+    }
+}
