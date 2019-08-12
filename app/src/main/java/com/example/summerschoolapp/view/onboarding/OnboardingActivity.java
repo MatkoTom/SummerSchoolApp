6831c0a301c78c8d6a7ca1552075638edc6a51c9
MatkoTom <tomljanovic.matko@gmail.com>
2019-08-12 10:59:49 +0200

    Added util for JWT, extracted some data...

--- app/src/main/java/com/example/summerschoolapp/view/onboarding/OnboardingActivity.java
+++ app/src/main/java/com/example/summerschoolapp/view/onboarding/OnboardingActivity.java
@@ -12,10 +12,8 @@ import com.example.summerschoolapp.R;
 import com.example.summerschoolapp.common.BaseActivity;
 import com.example.summerschoolapp.common.BaseError;
 import com.example.summerschoolapp.dialog.ErrorDialog;
-import com.example.summerschoolapp.model.BigDataResponse;
 import com.example.summerschoolapp.model.RequestLogin;
 import com.example.summerschoolapp.model.RequestRegister;
-import com.example.summerschoolapp.utils.JWTUtils;
 import com.example.summerschoolapp.utils.Tools;
 import com.example.summerschoolapp.utils.helpers.Event;
 import com.example.summerschoolapp.view.main.MainScreenActivity;
@@ -54,8 +52,6 @@ public class OnboardingActivity extends BaseActivity implements SignupFragment.O
         });
 
         Timber.d("IsUserSaved: %s", Tools.getSharedPreferences(this).getRememberMeStatus());
-
-        isUserRemembered();
     }
 
     public void runFirstLoginFragment() {
