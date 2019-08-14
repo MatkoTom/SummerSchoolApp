785e1f323269f8d455fff2f7f5e87907707f4a44
MatkoTom <tomljanovic.matko@gmail.com>
2019-08-14 15:59:01 +0200

    Added views to layouts, created some new drawables, did some designing...

--- app/src/main/java/com/example/summerschoolapp/view/main/fragmentRequests/RequestsFragment.java
+++ app/src/main/java/com/example/summerschoolapp/view/main/fragmentRequests/RequestsFragment.java
@@ -2,32 +2,59 @@ package com.example.summerschoolapp.view.main.fragmentRequests;
 
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.fragment.app.Fragment;
 
 import com.example.summerschoolapp.R;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class RequestsFragment extends Fragment {
 
+    @BindView(R.id.spinner_request_items)
+    Spinner spinner_requestItems;
+
+    @BindView(R.id.spinner_layout)
+    ConstraintLayout spinnerLayout;
+
+    @BindView(R.id.fab_create_new_request)
+    FloatingActionButton fabCreateNewRequest;
 
     public RequestsFragment() {
         // Required empty public constructor
     }
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View rootView = inflater.inflate(R.layout.fragment_requests, container, false);
+        ButterKnife.bind(this, rootView);
+
+        populateSpinner();
         return rootView;
+    }
+
+    private void populateSpinner() {
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(getActivity(), R.layout.spinner_item, getActivity().getResources().getStringArray(R.array.testArray));
+        spinner_requestItems.setAdapter(adapter);
+    }
+
+    @OnClick(R.id.fab_create_new_request)
+    public void createNewRequest() {
+
     }
 
 }
